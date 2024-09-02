0df3c9e4a427df805c3e89e0b0c227605a3f9a1e
Ashley Newman <ashleynew2122002@gmail.com>
2024-09-02 17:20:56 +0200

    models update

---
+++ db/migrate/20240902151332_create_items.rb
@@ -0,0 +1,13 @@
+class CreateItems < ActiveRecord::Migration[7.1]
+  def change
+    create_table :items do |t|
+      t.string :name
+      t.integer :price
+      t.string :category
+      t.text :description
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
